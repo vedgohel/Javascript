6f24299f73f55174060d1310f3c634462925a1c7
VedGohel <vedgohel85@gmail.com>
2025-07-10 23:44:40 +0530

    datatypes

---
+++ Basic/02_DataTypes.js
@@ -0,0 +1,24 @@
+"use strict";
+
+// alret( 3 + 4 ) // we are using node.js , not browser
+
+let name = "Ved";
+let age = 20;
+let isLoggedIn = false;
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true or false
+// null => empty value
+// undefined => variable is not defined
+// symbol => unique value   
+
+
+//object
+
+
+console.log(typeof name); // string
+console.log(typeof null); // object (this is a bug in JavaScript)
+console.log(typeof undefined); // undefined
