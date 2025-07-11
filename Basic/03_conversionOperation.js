7e856011f11c6b7ad0d8de91f0507241382cb5e0
VedGohel <vedgohel85@gmail.com>
2025-07-11 16:21:25 +0530

    comaprision and data type-summary

--- Basic/03_conversionOperation.js
+++ Basic/03_conversionOperation.js
@@ -31,3 +31,35 @@ let someNumber = 33;
 let stringNumber = String(someNumber);
 console.log(stringNumber); // 33
 console.log(typeof stringNumber); // string
+
+
+
+//  ---- operation -----
+
+let value = 3
+let negvalue = -value
+
+//console.log(negvalue); //-3
+
+/* 
+console.log(2+2);
+console.log(2-2);
+console.log(2+2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%2);
+*/
+
+
+let num1,num2,num3;
+
+num1 = num2 = num3 = 2 +2 ;
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter); 
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.
+//html#sec-type-conversion
