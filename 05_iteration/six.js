3a02fe97355a46167f740bb4d152b404a6dc945c
VedGohel <vedgohel85@gmail.com>
2025-07-15 17:29:01 +0530

    filter

---
+++ 05_iteration/six.js
@@ -0,0 +1,20 @@
+// // 
+
+ const num = [1 ,2 ,3, 4, 5, 6, 7,8,9,10]
+
+// // const newNums = myNums.filter( (num) =>{ 
+// //    return  num > 4 
+// // } )
+
+// const newNums = []
+
+// myNums.forEach( () => { 
+//     if (num > 4) {
+//         newNums.push(num)
+//     }
+// })
+
+// console.log(newNums);
+
+
+num > 4 
