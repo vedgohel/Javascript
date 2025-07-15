91dc328621d64e8b994173498f4c0679697201d2
VedGohel <vedgohel85@gmail.com>
2025-07-15 16:27:32 +0530

    for in loop

---
+++ 05_iteration/four.js
@@ -0,0 +1,19 @@
+// const myObject = {
+//     js : 'javascript',
+//     cpp: 'C++',
+//     rb : 'ruby',
+//     swift: 'swift for apple'
+// }
+
+// for (const key in myObject) {
+//    console.log(`${key} shortcut  is for ${myObject[key]}`);
+// }
+
+// const programming = ["ved" , "shivam" , "svayam" , "henish" , "monang"]
+
+// for (const key in programming) {
+//     console.log(`${programming[key]}`);
+    
+// }
+
+
