fa31fe94970cb1b7f0535feac546c37bb69ffd92
VedGohel <vedgohel85@gmail.com>
2025-07-15 17:28:46 +0530

    loop

---
+++ 05_iteration/five.js
@@ -0,0 +1,30 @@
+// // for each 
+
+// const programming = ["js" , "ruby" , "java" , "python" , "cpp"]
+
+// // programming.forEach( function name(val) {
+// //       console.log(val);
+// // })
+
+// // programming.forEach( (item) =>{
+// //     console.log(item);
+// // })
+
+// programming.forEach( (item , index  ,arr) =>{
+//         console.log(item ,index , arr);
+// })
+
+// const myCoding = [
+//     {
+//         languageName:"java",
+//         languageFileName:".java"
+//     },
+//     {
+//         languageName:"python",
+//         languageFileName:".py"
+//     },
+// ]
+
+// myCoding = ( (item) =>  {
+//     console.log(item.languageName);
+// })
