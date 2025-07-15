c8aa055b6f3d80bb6c754c8457394fbd06740b64
VedGohel <vedgohel85@gmail.com>
2025-07-15 16:26:09 +0530

    for of loop

---
+++ 05_iteration/three.js
@@ -0,0 +1,37 @@
+// for  of 
+
+// [ "" , "" , "" ]
+// [{} , {} , {} ]
+
+const arr = [1, 2, 3, 4, 5]
+//const num =1 ;
+for ( const num of arr ) {
+    // console.log(num);
+}
+
+const greetings = "Hello world"
+for (const greet of greetings) {
+    // console.log(`Each char is ${greet}`);
+}
+
+// map
+
+const map = new Map()
+map.set('IN' , "India")
+map.set('USA' , "United States of America")
+map.set('Fr' , "France")
+map.set('IN' , "India")
+
+
+// for (const [key , value] of map) {
+//     console.log(key , value);   
+// }
+
+// const myObject = {
+//     'game1' : 'NFS',
+//     'game2' : "Spiderman"
+// }
+
+// for (const [key , value] of map) {
+//     console.log(key , value);   
+// }
